e5fd1699747c4d9d2a3a5e3b8770ab38517ad5cd
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-03-08 08:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const container = document.querySelector(".container");
+
+for(let i=1;i<=10;i++){
+	const div = document.createElement('div');
+	div.id = `div${i}`;
+	div.innerText = i;
+	div.style.backgroundColor = 'green';
+	div.style.color = '#ffffff';
+	div.style.fontSize = '4vw';
+	container.appendChild(div);
+}
